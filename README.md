c08b316e9f38e21c76aa72945fede190112b590d
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2020-07-16 23:24:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # excelToFirestore
 Parse Excel file and upload to Firestore
+
+This was used for mass import of my local excel file book library to Firebase
---
+++ app.js
@@ -0,0 +1,54 @@
+const xlsxFile = require('read-excel-file/node')
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+const schema = {
+    'Book Title': {
+        prop: 'BookTitle',
+        type: String
+    },
+    'Author': {
+        prop: 'Author',
+        type: String,
+    },
+    'Owned': {
+        prop: 'Owned',
+        type: Boolean
+    },
+    'Category':{
+        prop: 'Category',
+        type: String
+    },
+    'Loan': {
+        prop: 'Loan',
+        type: Boolean
+    },
+    'ReadingNow': {
+        prop: 'ReadingNow',
+        type: Boolean
+    }
+}
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://my-bookshelf-d9b0e.firebaseio.com",
+    projectId: "my-bookshelf-d9b0e"
+  });
+  
+  const db = admin.firestore();
+
+xlsxFile('./Library.xlsx',{schema}).then(({rows,errors}) => {
+    errors.length === 0
+    rows.forEach(element => {
+        db.collection('Bookshelf').add({
+            Author: element.Author,
+            Title: element.BookTitle,
+            Owned:element.Owned,
+            Category:element.Category
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    });
+})
+.catch(err=>{
+    console.log(err)
+})
